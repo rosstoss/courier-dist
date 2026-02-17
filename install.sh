7203e1643f09bebdfa139b7a6817a595f147e543
Ryker Ross <ryker.ross@gmail.com>
2026-02-16 18:34:01 -0700

    Modified install script to address Mac quarantine issues

--- install.sh
+++ install.sh
@@ -4,6 +4,8 @@ set -e
 GITHUB_USER="rosstoss"
 GITHUB_REPO="courier-dist"
 BINARY_NAME="courier"
+INSTALL_DIR="$HOME/.courier"
+BIN_DIR="$INSTALL_DIR/bin"
 
 CYAN='\033[1;36m'
 BLUE='\033[1;34m'
@@ -13,12 +15,12 @@ NC='\033[0m'
 clear
 
 echo -e "${CYAN}"
-echo "   ______                 _          "
-echo "  / ____/___  __  _______(_)__  _____"
-echo " / /   / __ \/ / / / ___/ / _ \/ ___/"
-echo "/ /___/ /_/ / /_/ / /  / /  __/ /    "
-echo " \____/\____/\__,_/_/  /_/\___/_/    "
-echo -e "${BLUE}      INFRASTRUCTURE INSTALLER${NC}"
+echo "      ______                 _          "
+echo "     / ____/___   __  _______(_)__  _____"
+echo "    / /   / __ \/ / / / ___/ / _ \/ ___/"
+echo "   / /___/ /_/ / /_/ / /  / /  __/ /     "
+echo "   \____/\____/\__,_/_/  /_/\___/_/      "
+echo -e "${BLUE}       INFRASTRUCTURE INSTALLER${NC}"
 echo ""
 
 echo -e "${BLUE}[+]${NC} Checking System Compatibility..."
@@ -35,9 +37,6 @@ if ! command -v python3 &> /dev/null; then
     exit 1
 fi
 
-INSTALL_DIR="$HOME/.courier"
-BIN_DIR="$INSTALL_DIR/bin"
-
 mkdir -p "$BIN_DIR"
 
 SHELL_CONFIG="$HOME/.zshrc"
@@ -50,7 +49,13 @@ fi
 
 echo -e "${BLUE}[+]${NC} Fetching Courier Engine..."
 
-DOWNLOAD_URL="https://github.com/$GITHUB_USER/$GITHUB_REPO/releases/latest/download/$BINARY_NAME"
+if [ -z "$VERSION_TAG" ]; then
+    DOWNLOAD_URL="https://github.com/$GITHUB_USER/$GITHUB_REPO/releases/latest/download/$BINARY_NAME"
+else
+    DOWNLOAD_URL="https://github.com/$GITHUB_USER/$GITHUB_REPO/releases/download/$VERSION_TAG/$BINARY_NAME"
+fi
+
+echo "Downloading from: $DOWNLOAD_URL"
 
 if ! curl -# -L "$DOWNLOAD_URL" -o "$BIN_DIR/$BINARY_NAME"; then
     echo -e "${RED}Error: Download failed.${NC}"
@@ -62,10 +67,14 @@ echo -e "${BLUE}[+]${NC} Finalizing Installation..."
 
 chmod +x "$BIN_DIR/$BINARY_NAME"
 
+xattr -d com.apple.quarantine "$BIN_DIR/$BINARY_NAME" 2>/dev/null || true
+
 export PATH="$BIN_DIR:$PATH"
 
 echo -e "${BLUE}[+]${NC} Launching Setup Wizard..."
 sleep 1
+
+cd "$INSTALL_DIR"
 
 clear
 
